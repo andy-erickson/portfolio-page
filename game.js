38579d7f22e93eac9cbe3e58fd812c07fb5cf233
Andy <andyerickson58201@gmail.com>
2022-01-18 16:23:05 -0600

    cleaned up code, blocks and heights now generated with loop

--- game.js
+++ game.js
@@ -2,58 +2,69 @@ const gameState = {
   offWhite: 0xEEEEEE,
   lightTeal: 0xAAD8D3,
   teal: 0x00ADB5,
-  slate: 0x393E46
-};
+  slate: 0x393E46,
+  heights: [],
+  rects: []
+  };
 
 
 function create() {
-  let heights = [];
+  let xPosition = 65;
+  bubbleButton = this.add.rectangle(100,70,150,50,gameState.teal).setInteractive();
+  bubbleButton.on('pointerdown', function swap(gameState.heights[0], gameState.heights[1]));
   for (let i = 0; i < 10; i++) {
-    heights.push(Math.floor(Math.random() * 100));
-}
-
-  Math.floor(Math.random() * 10);
-  gameState.r1 = this.add.rectangle(65,300,40,heights[0],gameState.lightTeal).setInteractive();
-  gameState.r1.on('pointerover', function(){this.fillColor = gameState.teal;});
-  gameState.r1.on('pointerout', function(){this.fillColor = gameState.lightTeal;});
-  gameState.r2 = this.add.rectangle(125,300,40,heights[1],gameState.lightTeal).setInteractive();
-  gameState.r2.on('pointerover', function(){this.fillColor = gameState.teal;});
-  gameState.r2.on('pointerout', function(){this.fillColor = gameState.lightTeal;});
-  gameState.r3 = this.add.rectangle(185,300,40,heights[2],gameState.lightTeal).setInteractive();
-  gameState.r3.on('pointerover', function(){this.fillColor = gameState.teal;});
-  gameState.r3.on('pointerout', function(){this.fillColor = gameState.lightTeal;});
-  gameState.r4 = this.add.rectangle(245,300,40,heights[3],gameState.lightTeal).setInteractive();
-  gameState.r4.on('pointerover', function(){this.fillColor = gameState.teal;});
-  gameState.r4.on('pointerout', function(){this.fillColor = gameState.lightTeal;});
-  gameState.r5 = this.add.rectangle(305,300,40,heights[4],gameState.lightTeal).setInteractive();
-  gameState.r5.on('pointerover', function(){this.fillColor = gameState.teal;});
-  gameState.r5.on('pointerout', function(){this.fillColor = gameState.lightTeal;});
-  gameState.r6 = this.add.rectangle(365,300,40,heights[5],gameState.lightTeal).setInteractive();
-  gameState.r6.on('pointerover', function(){this.fillColor = gameState.teal;});
-  gameState.r6.on('pointerout', function(){this.fillColor = gameState.lightTeal;});
-  gameState.r7 = this.add.rectangle(425,300,40,heights[6],gameState.lightTeal).setInteractive();
-  gameState.r7.on('pointerover', function(){this.fillColor = gameState.teal;});
-  gameState.r7.on('pointerout', function(){this.fillColor = gameState.lightTeal;});
-  gameState.r8 = this.add.rectangle(485,300,40,heights[7],gameState.lightTeal).setInteractive();
-  gameState.r8.on('pointerover', function(){this.fillColor = gameState.teal;});
-  gameState.r8.on('pointerout', function(){this.fillColor = gameState.lightTeal;});
-  gameState.r9 = this.add.rectangle(545,300,40,heights[8],gameState.lightTeal).setInteractive();
-  gameState.r9.on('pointerover', function(){this.fillColor = gameState.teal;});
-  gameState.r9.on('pointerout', function(){this.fillColor = gameState.lightTeal;});
-  gameState.r10 = this.add.rectangle(605,300,40,heights[9],gameState.lightTeal).setInteractive();
-  gameState.r10.on('pointerover', function(){this.fillColor = gameState.teal;});
-  gameState.r10.on('pointerout', function(){this.fillColor = gameState.lightTeal;});
-}
+    gameState.heights.push(Math.ceil(Math.random() * 200));
+    gameState.rects.push(this.add.rectangle(xPosition,300,40,gameState.heights[i],gameState.lightTeal).setInteractive());
+    xPosition += 60;
+    }
+  }
 
 
 function update() {
-  gameState.r1.on('pointerdown', function(){this.y += this.getDistanceY();});
+  for (let i = 0; i < 10; i++) {
+    gameState.rects[i].height = gameState.heights[i];
+    gameState.rects[i].on('pointerover', function(){this.fillColor = gameState.teal;});
+    gameState.rects[i].on('pointerout', function(){this.fillColor = gameState.lightTeal;});
+    }
+  }
+
+  function swap(xp, yp){
+    let temp = gameState.heights[xp];
+    //var temp2 = gameState.rects[xp].height;
+    gameState.heights[xp] = gameState.heights[yp];
+    //gameState.rects[xp].height = gameState.rects[yp].height;
+    gameState.heights[yp] = temp;
+    //gameState.rects[yp].height = temp2;
+  }
+
+// An optimized version of Bubble Sort
+/*
+function bubbleSort( arr, n)
+{
+var i, j;
+for (i = 0; i < n-1; i++)
+{
+    for (j = 0; j < n-i-1; j++)
+    {
+
+        if (arr[j] > arr[j+1])
+        {
+        swap(arr,j,j+1);
+
+        }
+    }
+
 }
+}
+*/
+//}
 const config = {
   width: 700,
   height: 600,
   backgroundColor: gameState.slate,
-  scene: { create}
+  scene: { create, update
+
+           }
 }
 
 const game = new Phaser.Game(config)
